b8a7263eea2fe52e0376cd7a6808a18617288fb2
fenghsuisme <louishsu358@gmail.com>
2025-08-21 21:52:27 +0800

    ff

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	productionBrowserSourceMaps: true,
+	devIndicators: false, 
 };
 
 export default nextConfig;
